53715adca7cd3bcefded99701af1f8e5b49196a4
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-20 19:49:36 +0100

    enhance IconsNav component with additional menu items and improve CSS for better layout and hover effects

--- src/components/IconsNav.jsx
+++ src/components/IconsNav.jsx
@@ -4,7 +4,11 @@ export default function IconsNav() {
 
         <nav className="menu-icons">
             <ul>
-                <li><a href="#"><img src='/img/buy-comics-digital-comics.png' alt="digital comics" /></a> Digital comics</li>
+                <li><a href="#"><img src='/img/buy-comics-digital-comics.png' alt="digital comics" /><span className="icon-text">digital comics</span></a></li>
+                <li><a href="#"><img src='/img/buy-comics-merchandise.png' alt="merchandise" /><span className="icon-text">dc merchandise</span></a></li>
+                <li><a href="#"><img src='/img/buy-comics-subscriptions.png' alt="subscription" /><span className="icon-text">subscription</span></a></li>
+                <li><a href="#"><img src='/img/buy-comics-shop-locator.png' alt="shop locator" /><span className="icon-text">shop locator</span></a></li>
+                <li><a href="#"><img className="special-icon" src='/img/buy-dc-power-visa.svg' alt="shop locator" /><span className="icon-text">dc power visa</span></a></li>
             </ul>
         </nav>
 
