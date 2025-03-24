162e5e26bcc025c9a6734b6c51f904e461ceddf4
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-24 13:07:33 +0100

    refactor ComicsList and Card components; integrate comics data and update layout for improved rendering

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import styles from './Card.module.css'
 export default function Card({ image, title }) {
 
     return (
-        <div>
+        <div className="col-2">
             <div className={styles.card}>
                 <img src={image} alt={title} />
                 <div>{title}</div>
